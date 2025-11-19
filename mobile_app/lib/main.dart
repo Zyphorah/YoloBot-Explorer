55db075c82eedfefa4d97a3531424e4dcffa24ee
Zyphorah <zyphorah.zen@gmail.com>
2025-11-19 10:53:54 -0500

    Bloquer la rotation en mode paysage

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'screens/menu_staggered_animations.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(
     MaterialApp(
       home: MenuStaggeredAnimations(),
