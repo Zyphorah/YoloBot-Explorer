e8c1f7e8d6580cbcf904f2dc8b14ff2c273e609f
zyphorah <zyphorah.zen@gmail.com>
2025-12-03 00:48:48 +0000

    Ajouter dans le lamda un gestion de tim debut car il y avais un bug qui empêchais de tourner

--- robot_python/commande/command_handler.py
+++ robot_python/commande/command_handler.py
@@ -1,3 +1,5 @@
+import time
+
 class CommandHandler:
     
     def __init__(self, navigation, servo):
@@ -6,8 +8,8 @@ class CommandHandler:
         self._commandes = {
             "avancer": lambda: (self.nav.avancer(), {"action_courante": "avancer"})[1],
             "reculer": lambda: (self.nav.reculer(), {"action_courante": "reculer"})[1],
-            "gauche": lambda: (self.nav.tourner_gauche(), {"action_courante": "tourner_gauche_90"})[1],
-            "droite": lambda: (self.nav.tourner_droite(), {"action_courante": "tourner_droite_90"})[1],
+            "gauche": lambda: (self.nav.tourner_gauche(), {"action_courante": "tourner_gauche_90", "debut_rotation": time.time()})[1],
+            "droite": lambda: (self.nav.tourner_droite(), {"action_courante": "tourner_droite_90", "debut_rotation": time.time()})[1],
             "stop": lambda: (self.nav.arreter(), {"action_courante": "stop", "mode_autonome": False})[1],
             
             "cam_gauche": lambda: (self.servo.tourner(180), {})[1],
